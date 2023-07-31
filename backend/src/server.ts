43bfeeb923a6d09cd78e105f38c758c78db7e8e7
Frederico Tavares <fredericotp@hotmail.com>
2023-07-31 13:47:23 -0300

    Chore: ESLint airbnb

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,9 +3,9 @@ import app from './app';
 const PORT = process.env.PORT || 3001;
 
 const server = app.listen(PORT, () => {
-	console.log(
-		`Server is running on PORT: ${PORT}`,
-	);
+  console.log(
+    `Server is running on PORT: ${PORT}`,
+  );
 });
 
 export default server;
